78e25959e4d9887a32ede807e3f3cf873164377e
Ghanim Fodi <gfodi@codeaurora.org>
2017-01-24 15:42:30 +0200

    msm: ipa3: Validate IPA and GSI firmwares before loading
    IPA and GSI firmwares are saved on the file-system as an
    ELF file. IPA driver extracts the firmwares and load
    them during driver initialization.
    This change adds validation steps to each firmware before
    loading: load addresses, memory sizes, firmware sizes and
    more...
    
    Change-Id: I7d7f66e8e8a9ca0efae08b1e57b25ae4e44cc5bb
    CRs-fixed: 1110522
    Signed-off-by: Ghanim Fodi <gfodi@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal.c
+++ drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1052,6 +1052,22 @@ static void ipahal_debugfs_remove(void)
 static void ipahal_debugfs_init(void) {}
 static void ipahal_debugfs_remove(void) {}
 #endif /* CONFIG_DEBUG_FS */
+
+/*
+ * Get IPA Data Processing Star image memory size at IPA SRAM
+ */
+u32 ipahal_get_dps_img_mem_size(void)
+{
+	return IPA_HW_DPS_IMG_MEM_SIZE_V3_0;
+}
+
+/*
+ * Get IPA Header Processing Star image memory size at IPA SRAM
+ */
+u32 ipahal_get_hps_img_mem_size(void)
+{
+	return IPA_HW_HPS_IMG_MEM_SIZE_V3_0;
+}
 
 int ipahal_init(enum ipa_hw_type ipa_hw_type, void __iomem *base)
 {
