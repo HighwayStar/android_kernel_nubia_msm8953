000c9504a1651fd58d90b78f1bab232d080fae7b
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-26 22:40:12 +0800

    drivers: camera_v2: ifndef ir_cut and ir_led code for nx551j
    Change-Id: I938e8d0b1fda6eba42e22de2a67b9357a2b696d3

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -273,11 +273,14 @@ struct msm_flash_cfg_data_t32 {
 #define VIDIOC_MSM_FLASH_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 13, struct msm_flash_cfg_data_t32)
 
+#if !defined(CONFIG_MACH_NUBIA_NX551J) && !defined(CONFIG_MACH_NUBIA_NX549J)
 #define VIDIOC_MSM_IR_LED_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 14, struct msm_ir_led_cfg_data_t32)
 
 #define VIDIOC_MSM_IR_CUT_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t32)
+#endif
+
 #endif
 
 #endif
