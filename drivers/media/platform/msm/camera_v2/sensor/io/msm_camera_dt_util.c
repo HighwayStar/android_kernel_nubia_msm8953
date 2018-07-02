teik-off/android_kernel_lenovo_msm8953

drivers: camera_v2: ifndef ir_cut and ir_led code for tb8703
Change-Id: I938e8d0b1fda6eba42e22de2a67b9357a2b696d3

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
@@ -281,7 +281,7 @@ int msm_sensor_get_sub_module_index(struct device_node *of_node,
 		of_node_put(src_node);
 		src_node = NULL;
 	}
-
+#ifndef CONFIG_MACH_LENOVO_TB8703
 	src_node = of_parse_phandle(of_node, "qcom,ir-led-src", 0);
 	if (!src_node) {
 		CDBG("%s:%d src_node NULL\n", __func__, __LINE__);
@@ -313,7 +313,7 @@ int msm_sensor_get_sub_module_index(struct device_node *of_node,
 		of_node_put(src_node);
 		src_node = NULL;
 	}
-
+#endif
 	rc = of_property_read_u32(of_node, "qcom,strobe-flash-sd-index", &val);
 	if (rc != -EINVAL) {
 		CDBG("%s qcom,strobe-flash-sd-index %d, rc %d\n", __func__,

include/media/msm_cam_sensor.h
@@ -274,11 +274,14 @@ struct msm_flash_cfg_data_t32 {
 #define VIDIOC_MSM_FLASH_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 13, struct msm_flash_cfg_data_t32)
 
+#ifndef CONFIG_MACH_LENOVO_TB8703
 #define VIDIOC_MSM_IR_LED_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 14, struct msm_ir_led_cfg_data_t32)
 
 #define VIDIOC_MSM_IR_CUT_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t32)
+#endif
+
 #endif
 
 #endif

include/uapi/media/msm_cam_sensor.h
@@ -87,8 +87,10 @@ enum sensor_sub_module_t {
 	SUB_MODULE_CSIPHY_3D,
 	SUB_MODULE_OIS,
 	SUB_MODULE_EXT,
+#ifndef CONFIG_MACH_LENOVO_TB8703
 	SUB_MODULE_IR_LED,
 	SUB_MODULE_IR_CUT,
+#endif
 	SUB_MODULE_MAX,
 };
 
@@ -602,11 +604,13 @@ struct sensor_init_cfg_data {
 #define VIDIOC_MSM_OIS_CFG_DOWNLOAD \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 14, struct msm_ois_cfg_download_data)
 
+#ifndef CONFIG_MACH_LENOVO_TB8703
 #define VIDIOC_MSM_IR_LED_CFG \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_led_cfg_data_t)
 
 #define VIDIOC_MSM_IR_CUT_CFG \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t)
+#endif
 
 #endif
 

include/uapi/media/msmb_camera.h
@@ -52,9 +52,13 @@
 #define MSM_CAMERA_SUBDEV_SENSOR_INIT  14
 #define MSM_CAMERA_SUBDEV_OIS          15
 #define MSM_CAMERA_SUBDEV_FLASH        16
+#ifdef CONFIG_MACH_LENOVO_TB8703
+#define MSM_CAMERA_SUBDEV_EXT          17
+#else
 #define MSM_CAMERA_SUBDEV_IR_LED       17
 #define MSM_CAMERA_SUBDEV_IR_CUT       18
 #define MSM_CAMERA_SUBDEV_EXT          19
+#endif
 
 #define MSM_MAX_CAMERA_SENSORS  5
 

HighwayStar/android_kernel_nubia_msm8953

drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
@@ -287,7 +287,7 @@ int msm_sensor_get_sub_module_index(struct device_node *of_node,
 		of_node_put(src_node);
 		src_node = NULL;
 	}
-
+#ifndef CONFIG_MACH_NUBIA_NX551J
 	src_node = of_parse_phandle(of_node, "qcom,ir-led-src", 0);
 	if (!src_node) {
 		CDBG("%s:%d src_node NULL\n", __func__, __LINE__);
@@ -319,7 +319,7 @@ int msm_sensor_get_sub_module_index(struct device_node *of_node,
 		of_node_put(src_node);
 		src_node = NULL;
 	}
-
+#endif
 	rc = of_property_read_u32(of_node, "qcom,strobe-flash-sd-index", &val);
 	if (rc != -EINVAL) {
 		CDBG("%s qcom,strobe-flash-sd-index %d, rc %d\n", __func__,

include/media/msm_cam_sensor.h
@@ -274,11 +274,14 @@ struct msm_flash_cfg_data_t32 {
 #define VIDIOC_MSM_FLASH_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 13, struct msm_flash_cfg_data_t32)
 
+#ifndef CONFIG_MACH_NUBIA_NX551J
 #define VIDIOC_MSM_IR_LED_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 14, struct msm_ir_led_cfg_data_t32)
 
 #define VIDIOC_MSM_IR_CUT_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t32)
+#endif
+
 #endif
 
 #endif

include/uapi/media/msm_cam_sensor.h
@@ -97,8 +97,10 @@ enum sensor_sub_module_t {
 	SUB_MODULE_CSIPHY_3D,
 	SUB_MODULE_OIS,
 	SUB_MODULE_EXT,
+#ifndef CONFIG_MACH_NUBIA_NX551J
 	SUB_MODULE_IR_LED,
 	SUB_MODULE_IR_CUT,
+#endif
 	SUB_MODULE_MAX,
 };
 
@@ -667,11 +669,13 @@ struct sensor_init_cfg_data {
 #define VIDIOC_MSM_OIS_CFG_DOWNLOAD \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 14, struct msm_ois_cfg_download_data)
 
+#ifndef CONFIG_MACH_NUBIA_NX551J
 #define VIDIOC_MSM_IR_LED_CFG \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_led_cfg_data_t)
 
 #define VIDIOC_MSM_IR_CUT_CFG \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t)
+#endif
 
 #endif
 

include/uapi/media/msmb_camera.h
@@ -48,9 +48,13 @@
 #define MSM_CAMERA_SUBDEV_SENSOR_INIT  14
 #define MSM_CAMERA_SUBDEV_OIS          15
 #define MSM_CAMERA_SUBDEV_FLASH        16
+#ifdef CONFIG_MACH_NUBIA_NX551J
+#define MSM_CAMERA_SUBDEV_EXT          17
+#else
 #define MSM_CAMERA_SUBDEV_IR_LED       17
 #define MSM_CAMERA_SUBDEV_IR_CUT       18
 #define MSM_CAMERA_SUBDEV_EXT          19
+#endif
 
 #define MSM_MAX_CAMERA_SENSORS  5
 