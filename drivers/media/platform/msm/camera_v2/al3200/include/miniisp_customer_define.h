2aef2dc421aec4a1710386aee28c7099045ca7d9
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-09-03 23:41:52 +0800

    drivers: camera_v2: Load firmwares from /vendor
    * Load nubia dual camera isp firmware from /vendor

--- drivers/media/platform/msm/camera_v2/al3200/include/miniisp_customer_define.h
+++ drivers/media/platform/msm/camera_v2/al3200/include/miniisp_customer_define.h
@@ -5,13 +5,13 @@
 
 /******Public Define******/
 /*boot file location*/
-#define BOOT_FILE_LOCATION "/system/etc/firmware/miniBoot.bin"
+#define BOOT_FILE_LOCATION "/vendor/firmware/miniBoot.bin"
 /*basic code location*/
-#define BASIC_FILE_LOCATION "/system/etc/firmware/TBM_D2.bin"
+#define BASIC_FILE_LOCATION "/vendor/firmware/TBM_D2.bin"
 /*advanced code location*/
 #define ADVANCED_FILE_LOCATION NULL
 /*scenario table location*/
-#define SCENARIO_TABLE_FILE_LOCATION "/system/etc/firmware/SCTable.asb"
+#define SCENARIO_TABLE_FILE_LOCATION "/vendor/firmware/SCTable.asb"
 
 /*miniISP dump info save location*/
 /*Add location folder where you let Altek debug info saving in your device*/
